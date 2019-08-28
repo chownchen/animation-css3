3549a6d800c83b67945af6f47c8ad0a14167a069
chenzhangdong <chenzhangdong@xiaomi.com>
2019-08-29 01:02:46 +0800

    first commit on this time, it has a litter function, i should do more

---
+++ index.js
@@ -0,0 +1,62 @@
+// creat by zdchen
+
+window.onload = function () {
+  let imgWrap = document.querySelector('.imgList')
+  initPage(imgWrap)
+  mouseFunc(imgWrap)
+}
+// 是否正在拖动
+let mouseAbout = false
+// 定时触发旋转计算单位
+let piece = 0
+// 当前旋转值
+let currRotate = 0
+
+const initPage = (imgWrap) => {
+  let imgList = imgWrap.querySelectorAll('.img')
+  let len = imgList.length
+
+  for (var i = 0; i < len; i++) {
+    imgList[i].style.transform = `rotateY(${45 * i}deg) translateZ(500px)`;
+    imgList[i].style.transition = `1s linear ${(len - 1 - i)*0.2}s`;
+  }
+
+  window.setInterval(function () {
+    // 如果正在推拽，停止定时器触发运动
+    if (mouseAbout) return false
+  
+    piece++
+    imgWrap.style.transform = `rotateY(${currRotate + piece * 0.2}deg)`
+  }, 30)
+}
+
+const mouseFunc = (imgWrap) => {
+  let firstX = 0
+  let currX = 0
+  document.onmousedown = (e) => {
+    // 拖拽开始时，获取当前rotat值，便于拖拽平稳进行
+    mouseAbout = true
+    let currRotate = getRotate(imgWrap)
+    firstX = e.clientX
+
+
+    imgWrap.onmousemove = (e) => {
+      currX = e.clientX
+      const length = currX - firstX
+      imgWrap.style.transform = `rotateY(${currRotate + length * 0.1}deg)`
+    }
+  }
+  document.onmouseup = (e) => {
+    // 拖拽完毕时，获取当前rotate值，便于定时器运动平稳性
+    mouseAbout = false
+    currRotate = getRotate(imgWrap)
+    piece = 0
+    imgWrap.onmousemove = null
+  }
+}
+
+// 获取当前rotate的值
+const getRotate = ($el) => {
+  let str = $el && $el.style.transform
+  return str && +str.split('(')[1].split('deg)')[0]
+}
