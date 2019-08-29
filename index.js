b337917f0d540866ac2369d1ea806dc0a22f78b0
chenzhangdong <chenzhangdong@xiaomi.com>
2019-08-29 13:48:04 +0800

    fix：solve the compatible problem on chrome with 3D-transform

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // creat by zdchen
 
 window.onload = function () {
-  let imgWrap = document.querySelector('.imgList')
+  let imgWrap = document.querySelector('.infoList')
   initPage(imgWrap)
   mouseFunc(imgWrap)
 }
@@ -13,20 +13,22 @@ let piece = 0
 let currRotate = 0
 
 const initPage = (imgWrap) => {
-  let imgList = imgWrap.querySelectorAll('.img')
+  let imgList = imgWrap.querySelectorAll('.item')
   let len = imgList.length
 
-  for (var i = 0; i < len; i++) {
-    imgList[i].style.transform = `rotateY(${45 * i}deg) translateZ(500px)`;
-    imgList[i].style.transition = `1s linear ${(len - 1 - i)*0.2}s`;
+  for (let i = 0; i < len; i++) {
+    imgList[i].style.transform = `rotateY(${45 * i}deg) translateZ(500px)`
+    imgList[i].style.webkitTransform = `rotateY(${45 * i}deg) translateZ(500px)`
+    imgList[i].style.transition = `1s linear ${(len - 1 - i)*0.2}s`
+    imgList[i].style.webkitTransition = `1s linear ${(len - 1 - i)*0.2}s`
   }
 
   window.setInterval(function () {
     // 如果正在推拽，停止定时器触发运动
     if (mouseAbout) return false
-  
     piece++
-    imgWrap.style.transform = `rotateY(${currRotate + piece * 0.2}deg)`
+    imgWrap.style.transform = `rotateY(${currRotate + piece * 0.4}deg)`
+    imgWrap.style.webkitTransform = `rotateY(${currRotate + piece * 0.4}deg)`
   }, 30)
 }
 
@@ -39,13 +41,14 @@ const mouseFunc = (imgWrap) => {
     let currRotate = getRotate(imgWrap)
     firstX = e.clientX
 
-
     imgWrap.onmousemove = (e) => {
       currX = e.clientX
       const lengthX = currX - firstX
       imgWrap.style.transform = `rotateY(${currRotate + lengthX * 0.1}deg)`
+      imgWrap.style.webkitTransform = `rotateY(${currRotate + lengthX * 0.1}deg)`
     }
   }
+
   document.onmouseup = (e) => {
     // 拖拽完毕时，获取当前rotate值，便于定时器运动平稳性
     mouseAbout = false
